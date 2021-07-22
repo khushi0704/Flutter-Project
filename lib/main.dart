47bd8d875e45244d79a19ac2a73ef30edd58b072
Khushi Jain <khushi.jain0704@gmail.com>
2021-07-22 22:22:37 +0530

    day5

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/HomePg.dart';
+import 'pages/LoginPg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme:GoogleFonts.latoTextTheme()
+        ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+     // initialRoute:"/home",
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+      },
+    );
+  }
+
+ 
+  
+}
